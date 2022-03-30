2bc384256e0686752991869a8fd6f821ed61573e
Kai Lyons <kai@potabi.com>
2022-03-29 19:53:53 -0600

    Experiment to see why the configuration fails

--- interface/client.js
+++ interface/client.js
@@ -33,13 +33,14 @@ function printDisks(list, currentID, nextID, tableID, configName) {
         data = data_list[i].split(":")
         var newHtml =  `<tr class="table-row w-full">
                             <td class="table-cell">
-                                <a onclick="changePage('${nextID}', '${currentID}'); updateConfig('${configName}','${data[0].trim()}');">
+                                <a onclick="changePage('${nextID}', '${currentID}'); updateConfig('${configName}','${data[0]}');">
                                     ${data[0]} | ${data[1]}
                                 </a>
                             </td>
                         </tr>
                         `
         document.getElementById(tableID).innerHTML += newHtml;
+        system.exec(`echo ${configName}`)
     }
 }
 
