a16418d72f47a1de0e71812c73d01b9898445349
Kai Lyons <kai@potabi.com>
2022-03-29 23:55:17 -0600

    Start working on installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,7 @@ zpool create install-potabi /dev/${disk}
 zfs set mountpoint=/mnt/install install-potabi
 zfs set compression=gzip-6 install-potabi
 
+# Unpack base and kernel into directory
 
 # Unmount at end of script
 zfs unmount -f /mnt/install
